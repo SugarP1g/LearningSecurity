6c33c5f0b76deb915cd59cca8273fa144ea70bcd
SugarP1g <zhuchenhui1990@163.com>
2020-08-04 16:27:16 +0800

    Create Person.java

---
+++ src/JavaSecurity/src/main/java/com/SugarP1g/deserialization/XStream/Person.java
@@ -0,0 +1,10 @@
+package com.SugarP1g.deserialization.XStream;
+
+public class Person implements IPerson {
+    String name;
+    int age;
+
+    public void output() {
+        System.out.print("Hello, this is " + this.name + ", age " + this.age);
+    }
+}
