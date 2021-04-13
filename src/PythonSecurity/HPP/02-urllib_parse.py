fa3d6b4a63b34b1ab90144bd59c571ad92a67721
sugarP1G <zhuchenhui1990@163.com>
2021-04-13 16:34:56 +0800

    Create 02-urllib_parse.py

---
+++ src/PythonSecurity/HPP/02-urllib_parse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
