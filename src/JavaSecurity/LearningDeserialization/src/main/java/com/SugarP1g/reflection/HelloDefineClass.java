3b9143fcccaf0f0dbd2ccb0f1326c64b03fe313f
sugarP1G <zhuchenhui1990@163.com>
2021-09-14 11:47:55 +0800

    Create HelloDefineClass.java

---
+++ src/JavaSecurity/LearningDeserialization/src/main/java/com/SugarP1g/reflection/HelloDefineClass.java
@@ -0,0 +1,15 @@
+package com.SugarP1g.reflection;
+
+import java.lang.reflect.Method;
+import java.util.Base64;
+
+public class HelloDefineClass {
+    public static void main(String[] args) throws Exception {
+        Method defineClass = ClassLoader.class.getDeclaredMethod(
+                "defineClass", String.class, byte[].class, int.class, int.class);
+        defineClass.setAccessible(true);
+        byte[] code = Base64.getDecoder().decode("yv66vgAAADQAGwoABgANCQAOAA8IABAKABEAEgcAEwcAFAEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAApTb3VyY2VGaWxlAQAKSGVsbG8uamF2YQwABwAIBwAVDAAWABcBAAtIZWxsbyBXb3JsZAcAGAwAGQAaAQAFSGVsbG8BABBqYXZhL2xhbmcvT2JqZWN0AQAQamF2YS9sYW5nL1N5c3RlbQEAA291dAEAFUxqYXZhL2lvL1ByaW50U3RyZWFtOwEAE2phdmEvaW8vUHJpbnRTdHJlYW0BAAdwcmludGxuAQAVKExqYXZhL2xhbmcvU3RyaW5nOylWACEABQAGAAAAAAABAAEABwAIAAEACQAAAC0AAgABAAAADSq3AAGyAAISA7YABLEAAAABAAoAAAAOAAMAAAACAAQABAAMAAUAAQALAAAAAgAM");
+        Class hello = (Class) defineClass.invoke(ClassLoader.getSystemClassLoader(), "Hello", code, 0, code.length);
+        hello.newInstance();
+    }
+}
